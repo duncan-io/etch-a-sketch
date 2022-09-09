b914c885fccbfa23bd2affee51f455e337cce560
duncan <dmckee95@gmail.com>
2022-09-09 16:23:12 -0400

    Fix JS so boxes fill parent, adjust styling

--- scripts.js
+++ scripts.js
@@ -3,13 +3,14 @@ const etchArea = document.getElementById("etchArea");
 
 //Player amount of squares selection variable. 
 //**Still no way for the player to decide on number of squares**
-let playerProvidedSquares = 16
+let playerProvidedSquares = 100
 
 //Figure out how many squares will be used
 let numSquares = Math.pow(playerProvidedSquares, 2);
 
 //Adjust CSS grid to have appropriate amount of columns so etch box has the same amount of columns as rows
-etchArea.style.cssText=`grid-template-columns:repeat(${playerProvidedSquares}, 10px)`
+etchArea.style.gridTemplateColumns = `repeat(${playerProvidedSquares}, 1fr)`
+etchArea.style.gridTemplateRows = `repeat(${playerProvidedSquares}, 1fr)`
 
 //Create grid element divs and append them to the etchArea
 for (let i=0; i < numSquares; i++){
