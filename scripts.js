a253c215d74e99b1c28bad358e275b295d513cd1
duncan <dmckee95@gmail.com>
2022-09-09 11:43:07 -0400

    Add styling to etch area

--- scripts.js
+++ scripts.js
@@ -1,28 +1,29 @@
 
 const etchArea = document.getElementById("etchArea");
 
-//Player amount of squares selection variable. Still no way for the player to decide on number of squares
+//Player amount of squares selection variable. 
+//**Still no way for the player to decide on number of squares**
 let playerProvidedSquares = 16
+
 //Figure out how many squares will be used
 let numSquares = Math.pow(playerProvidedSquares, 2);
 
-//Append appropriate amount of squares so etch box has the right amount
-etchArea.style.cssText=`grid-template-columns:repeat(${playerProvidedSquares},100px)`
+//Adjust CSS grid to have appropriate amount of columns so etch box has the same amount of columns as rows
+etchArea.style.cssText=`grid-template-columns:repeat(${playerProvidedSquares}, 10px)`
 
-//Create divs and append them to the etchArea
+//Create grid element divs and append them to the etchArea
 for (let i=0; i < numSquares; i++){
-
 let newGridItem = document.createElement("div");
 newGridItem.classList.add("gridElement");
-newGridItem.textContent = i;
 etchArea.appendChild(newGridItem);
-
 }
 
 
-//Select all grid elements and add a mouseover event to them that changes their CSS class
+//Select all grid elements
+const gridElement = document.querySelectorAll(".gridElement")
 
-document.querySelectorAll(".gridElement").forEach(item => {item.addEventListener("mouseover", (event) => {event.target.style.backgroundColor = "black"})})
+//Add a mouseover event to them that changes their CSS class
+gridElement.forEach(element => {element.addEventListener("mouseover", (event) => {event.target.style.backgroundColor = "black"})});
 
 
 
